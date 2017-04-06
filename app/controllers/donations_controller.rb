930826fabed7438f36fa6f9954c89b71983e7689
kbanico <code4uguam@gmail.com>
2017-04-06 13:29:13 +1000

    add icon to top right of views

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,20 +3,24 @@ class DonationsController < ApplicationController
   def create
     @wish = Wish.find(params[:wish_id])
 
-    #update wish coin bucket
-    new_amount =@wish.coin_bucket + donation_params[:amount].to_i
-    @wish.update_attributes(coin_bucket: new_amount)
+    if current_user.coins >= donation_params[:amount].to_i
+      #update wish coin bucket
+      new_amount =@wish.coin_bucket + donation_params[:amount].to_i
+      @wish.update_attributes(coin_bucket: new_amount)
 
-    #update user coins
-    new_user_coins = current_user.coins-donation_params[:amount].to_i
-    current_user.update_attributes(coins: new_user_coins)
+      #update user coins
+      new_user_coins = current_user.coins-donation_params[:amount].to_i
+      current_user.update_attributes(coins: new_user_coins)
 
-    #make the donation for viewing purpose later
-    @wish.donations.create(donation_params.merge(user: current_user))
-    # current_user.update_attributes(coins: new_user_coins)
-    # @wish.update_attributes(coin_bucket: @wish.coin_bucket+new_amount)
+      #make the donation for viewing purpose later
+      @wish.donations.create(donation_params.merge(user: current_user))
+      # current_user.update_attributes(coins: new_user_coins)
+      # @wish.update_attributes(coin_bucket: @wish.coin_bucket+new_amount)
 
-    redirect_to wish_path(@wish)
+      redirect_to wish_path(@wish)
+    else
+      # maybe redirect them to a page to request for more coins
+    end
   end
 
 
