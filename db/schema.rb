d365c6a413a5f3b73097a9b70dfe0cb778ef8109
kbanico <code4uguam@gmail.com>
2017-04-06 20:35:13 +1000

    add username to user model add font-icon to submit donation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404090607) do
+ActiveRecord::Schema.define(version: 20170406101253) do
 
   create_table "donations", force: :cascade do |t|
     t.integer  "amount"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170404090607) do
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
     t.integer  "coins",                  default: 100
+    t.text     "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
