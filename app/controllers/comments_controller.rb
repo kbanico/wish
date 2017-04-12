660ecfa66fc60d9c8211ecdff856a248cb670258
kbanico <code4uguam@gmail.com>
2017-04-12 19:38:36 +1000

    move comment form to new.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+
+  def new
+    @wish = Wish.find(params[:wish_id])
+    @comment = Comment.new
+  end
+
   def create
     @wish = Wish.find(params[:wish_id])
     @wish.comments.create(comment_params.merge(user: current_user))
