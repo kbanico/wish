e5265bd25c8e118c35cca72320e1d5c69ad890d3
kbanico <code4uguam@gmail.com>
2017-04-27 18:27:57 +1000

    add latest wishes, latest replies to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @wishes = Wish.order("created_at ASC").last(6)
+    @comments = Comment.order("created_at ASC").last(6)
   end
 end
