ce464e39e28ade6cec0ca9fdaffd4da2be42e4a4
kbanico <code4uguam@gmail.com>
2017-05-03 18:16:06 +1000

    uploading feature is working with admin

--- app/controllers/upload_photos_controller.rb
+++ app/controllers/upload_photos_controller.rb
@@ -2,19 +2,43 @@ class UploadPhotosController < ApplicationController
 
   def index
     @photos = Photo.all
+    @user = current_user
+    if current_user && current_user.admin == true
+      @checkuser = "USER IS ADMIN"
+    else
+      @checkuser = "USER IS NOT ADMIN"
+    end
   end
 
 
   def new
-
+    @photo = Photo.new
   end
 
   def create
 
+    if current_user && current_user.admin == true
+      @photo = current_user.photos.create(photo_params)
+      if @photo.errors.any?
+        flash[:alert] = "THERE WAS AN ERROR"
+        redirect_to root_path
+      else
+        flash[:alert] = "Everything ok"
+        redirect_to :back
+      end
+    else
+      flash[:alert] = "SORRY YOUR NOT ADMIN"
+      redirect_to root_path
+    end
   end
 
 
   def destroy
 
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:caption, :picture)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428083807) do
+ActiveRecord::Schema.define(version: 20170430023637) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "message"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170428083807) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
